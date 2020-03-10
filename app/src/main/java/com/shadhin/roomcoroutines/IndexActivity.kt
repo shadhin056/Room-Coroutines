4f6be36022a3cb4079878c6ae74b5536e2a1748b
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-03-10 09:16:41 +0600

    init

---
+++ app/src/main/java/com/shadhin/roomcoroutines/IndexActivity.kt
@@ -0,0 +1,26 @@
+package com.shadhin.roomcoroutines
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class IndexActivity : AppCompatActivity() {
+    private lateinit var btnRoom: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index)
+        init();
+        buttonAction();
+    } private fun init() {
+        btnRoom = findViewById(R.id.btnRoom) as Button
+    }
+
+    private fun buttonAction() {
+
+        btnRoom.setOnClickListener {
+            val intent = Intent(this, RoomActivity::class.java)
+            startActivity(intent);
+        };
+    }
+}
