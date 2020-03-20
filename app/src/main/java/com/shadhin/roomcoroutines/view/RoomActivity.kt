3ba45ee148a75a84b24fd6c755dff10fe316b9ef
Admin <admin@Admins-MacBook-Pro.local>
2020-03-20 14:30:25 +0600

    3 fragment added

--- app/src/main/java/com/shadhin/roomcoroutines/view/RoomActivity.kt
+++ app/src/main/java/com/shadhin/roomcoroutines/view/RoomActivity.kt
@@ -1,12 +1,15 @@
-package com.shadhin.roomcoroutines
+package com.shadhin.roomcoroutines.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.shadhin.roomcoroutines.R
 
 class RoomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
     }
 }
